7ec412a782934b34ca90086436e1ee9716bba1c1
shelldan <shelldanlee@gmail.com>
2022-12-29 19:45:24 -0600

    add experience markup to component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'
+import Experience from './components/experience/Experience'
 
 const App = () => {
     return (
@@ -9,6 +10,7 @@ const App = () => {
         <Header/>
         <Nav />
         <About />
+        <Experience />
         </>
     )
 }
