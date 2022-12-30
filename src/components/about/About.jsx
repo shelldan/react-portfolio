457447f752078be0389b4a70ce803054eee9c55b
shelldan <shelldanlee@gmail.com>
2022-12-29 19:39:02 -0600

    add about style to component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import './about.css'
 import ME from '../../assets/me-about.jpeg'
 import { FaAward } from 'react-icons/fa'
-import {FiUsers} from 'react-icons/fi'
-import {VscFolderLibrary} from 'react-icons/vsc'
+import { FiUsers} from 'react-icons/fi'
+import { VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <section>
+    <section id="about">
       <h5>Get to Know</h5>
       <h2>About Me</h2>
 
@@ -21,29 +21,29 @@ const About = () => {
         <div className="about__content">
           <div className="about__cards">
             <article className="about__card">
-              <FaAward/>
+              <FaAward className='about__icon'/>
               <h5>Experience</h5>
               <small>Junior software engineer</small>
             </article>
 
             <article className="about__card">
-              <FiUsers/>
+              <FiUsers className='about__icon'/>
               <h5>Clients</h5>
               <small>200+ Worldwide</small>
             </article>
 
             <article className="about__card">
-              <VscFolderLibrary/>
+              <VscFolderLibrary className='about__icon'/>
               <h5>Projects</h5>
               <small>20+ Completed</small>
             </article>
+          </div>
 
             <p>
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius libero, debitis earum ipsa recusandae labore praesentium, dolore rem fugiat ex natus accusantium beatae, repudiandae repellendus? Odio maiores non dolore adipisci.
             </p>
 
             <a href="contact" className='btn btn-primary'>Let's Talk</a>
-          </div>
         </div>
       </div>
     </section>
