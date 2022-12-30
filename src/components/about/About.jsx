fbae11e3c937c42f61f5c68c86649083faa37353
shelldan <shelldanlee@gmail.com>
2022-12-29 19:15:53 -0600

    add about to component;

---
+++ src/components/about/About.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import './about.css'
+import ME from '../../assets/me-about.jpeg'
+import { FaAward } from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
+
+const About = () => {
+  return (
+    <section>
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="about-me Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward/>
+              <h5>Experience</h5>
+              <small>Junior software engineer</small>
+            </article>
+
+            <article className="about__card">
+              <FiUsers/>
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+
+            <article className="about__card">
+              <VscFolderLibrary/>
+              <h5>Projects</h5>
+              <small>20+ Completed</small>
+            </article>
+
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius libero, debitis earum ipsa recusandae labore praesentium, dolore rem fugiat ex natus accusantium beatae, repudiandae repellendus? Odio maiores non dolore adipisci.
+            </p>
+
+            <a href="contact" className='btn btn-primary'>Let's Talk</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default About
